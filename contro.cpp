2e297277d6b3b3bc719de68e4b67c354363129de
Quocanh289 <160709137+Quocanh289@users.noreply.github.com>
2024-04-16 10:30:16 +0700

    Add files via upload

---
+++ contro.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+//
+//int * tro(int a, int b){
+//int c;
+//c= a+b;
+//return &(a+b);
+//}
+// Bai 2:
+//int mang ( int*a, int s){
+//cout <<s;
+//return 0;
+//}
+// khi cho vao ham thi khong the
+
+//Bai 3
+//int chan ( int *a , int n){
+//    for( int i =0; i< n; i++){
+//        if( a[i]%2==0){
+//            cout << a[i]<< " ";
+//        }
+//    }
+//    return 0;
+//}
+int main (){
+    //Bai 1:
+    // loi chua con tro chua khoi tao
+//    int *p;
+//    cout << p;
+    // in ra mot con tro bat ky
+    // loi truy cap con tro null
+//    int *p = NULL;
+//    cout << p << *p;
+// truy cap vao con tro 0 trong bo nho
+// Bai 2
+//int n;
+//cin >> n;
+//
+//int a[n];
+//
+////cout <<  sizeof(a);// in ra kich thuoc = 4*n;
+//cout << mang(a, sizeof(a));
+//// Khong the khai bao mang thay vi con tro o trong ham
+
+// Bai 3
+//int n;
+//cin >> n;
+//int a[n];
+//for( int i =0; i< n; i++){
+//   cin >> a[i];
+//}
+//cout << chan(a,n);
+
+//Bai 4
+
+return 0;}
