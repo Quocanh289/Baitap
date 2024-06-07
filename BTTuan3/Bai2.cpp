b0e6e26d629b1c2f557fda3bb27a279dfc69faac
Quocanh289 <160709137+Quocanh289@users.noreply.github.com>
2024-06-07 20:11:13 +0700

    Add files via upload

---
+++ BTTuan3/Bai2.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std;
+struct Point {
+    int x, y;
+};
+void print (Point p) // khi dung ham khong can khai bao bien
+{
+    cout << p.x << p.y;
+}
+void tong ( Point p)
+{
+  p.x = p.x + p.y;
+}
+void hieu ( Point &p)
+{
+p.x = p.x - p.y;
+}
+int main ()
+{
+  Point point;
+  point.x = 10;
+  point.y =12;
+  tong (point);
+  print (point);
+ hieu(point);
+ print(point);
+  return 0;
+}
+//=> Khi truyen tham chieu vao thi no tuong tu nhu truyen 1 bien
